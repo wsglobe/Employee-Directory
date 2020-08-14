f69a1031d3fb1c1e8d0779833349a40203a5c183
Walter Wang <wsglobe@gmail.com>
2020-08-14 16:33:18 -0500

    Updates

---
+++ precache-manifest.a965bb9b27f0d586b13972037b8320ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b2675fb7f0b9541671ef5fb411510a0",
+    "url": "/Employee-Directory/index.html"
+  },
+  {
+    "revision": "6c19b1af9b38766ab50f",
+    "url": "/Employee-Directory/static/css/main.23c1ea5d.chunk.css"
+  },
+  {
+    "revision": "b60a091f75a00a1c0833",
+    "url": "/Employee-Directory/static/js/2.e5128ba5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Employee-Directory/static/js/2.e5128ba5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c19b1af9b38766ab50f",
+    "url": "/Employee-Directory/static/js/main.7ddda662.chunk.js"
+  },
+  {
+    "revision": "b8bb8fcf5be5137b6d2c",
+    "url": "/Employee-Directory/static/js/runtime-main.335ffeae.js"
+  }
+]);
